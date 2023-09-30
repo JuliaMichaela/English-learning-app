23a4b1d7f82ef347350b4bbeef9156f0fe34e0a1
JuliaMichaela <juliaperez@yandex.ru>
2023-09-30 23:30:05 +0300

    changed App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
+import Table from './components/Table/Table';
+import Wordcard from './components/WordCard/Wordcard';
 import './style/App.css';
 
 function App() {
   return (
     <div className="App">
-    
+    <Header/>
+    <Table/>
+    <Wordcard/>
+    <Footer/>
     </div>
   );
 }
