191dc66eb1050aedb6706ce3f9c566f94b0ba0a9
JuliaMichaela <juliaperez@yandex.ru>
2023-10-08 16:11:46 +0300

    edited Table.jsx

--- src/components/Table/Table.jsx
+++ src/components/Table/Table.jsx
@@ -1,8 +1,31 @@
 import './Table.css'
 import data from '../../assets/data.json'
+import TableRow from '../TableRow/TableRow'
 
 export default function Table() {
     return (
-        <div>Table</div>
+        <div className="table">
+            <table>
+                <thead>
+                    <tr>
+                        <th>Слово</th>
+                        <th>Транскрипция</th>
+                        <th>Перевод</th>
+                        <th>Кпопки</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        data.map((item) =>
+                            <TableRow
+                                isEdited={false}
+                                key={item.id}
+                                engVersion={item.english}
+                                rusVersion={item.russian}
+                                transcription={item.transcription}>
+                            </TableRow>)}
+                </tbody>
+            </table>
+        </div>
     )
 }
