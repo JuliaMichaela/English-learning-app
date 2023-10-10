2ea8214604739dd256adb2a9997e629230c628c8
JuliaMichaela <juliaperez@yandex.ru>
2023-10-10 23:19:17 +0300

    edited WordCardMap

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
+import React from 'react';
 import Footer from './components/Footer/Footer';
 import Header from './components/Header/Header';
 import Table from './components/Table/Table';
-import WordCard from './components/WordCard/WordCard'
+import WordCardMap from './components/WordCardMap/WordCardMap'
 import './style/App.css';
+
+import words from './assets/data.json';
 
 function App() {
   return (
     <div className="App">
     <Header/>
     <Table/>
-    <WordCard/>
+    <WordCardMap
+      words={words}
+      position={0}/>
     <Footer/>
     </div>
   );
--- src/components/WordCardMap/WordCardMap.jsx
+++ src/components/WordCardMap/WordCardMap.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import WordCard from '../WordCard/Wordcard'
 
 const WordCardMap = (props) => {
