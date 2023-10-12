01430bdb11823baa5d5dfdb7670a9cc2d2e06b14
JuliaMichaela <juliaperez@yandex.ru>
2023-10-12 23:06:54 +0300

    added slider

--- src/components/WordCardMap/WordCardMap.jsx
+++ src/components/WordCardMap/WordCardMap.jsx
@@ -14,13 +14,38 @@ const WordCardMap = (props) => {
         };
     }
 
-    const handlePrev = () => {
-        if (currentIndex > 0) {
-            setIndex(currentIndex - 1)
-        } else {
-            setIndex(words.length - 1)
+
+    import './WordCard.css'
+    import React, { useState } from "react";
+
+    export default function WordCard(props) {
+
+        const [isVisible, setVisible] = useState(false);
+        // const [index, setIndex] = useState(0);
+
+        const handleShow = () => {
+            setVisible(!isVisible);
+            props.addCount();
         };
-    }
+
+        // const handleIndex = () => {
+        //     setIndex(currenIndex => currenIndex + 1);
+        // }
+        // const mainRef = useRef(null);
+        // useEffect(() => {
+        //     mainRef.current.focus();
+        // }, []);
+
+        return (
+            <div className="card animation">
+                <div className="word">{props.engVersion}</div>
+                <div className="transcription">{props.transcription}</div>
+                <button className={isVisible ? "btnVisible word" : "btnTranslate"} disabled={isVisible} onClick={handleShow} >
+                    {isVisible ? props.rusVersion : "Проверить"}
+                </button>
+            </div >
+        );
+    };
 
     return (
         <div className="card-container">
