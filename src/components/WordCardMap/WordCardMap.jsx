f02221d70899cab8d30e5c4a52b707106b90c3b0
JuliaMichaela <juliaperez@yandex.ru>
2023-10-14 23:12:27 +0300

    edited WCM

--- src/components/WordCardMap/WordCardMap.jsx
+++ src/components/WordCardMap/WordCardMap.jsx
@@ -15,37 +15,7 @@ const WordCardMap = (props) => {
     }
 
 
-    import './WordCard.css'
-    import React, { useState } from "react";
 
-    export default function WordCard(props) {
-
-        const [isVisible, setVisible] = useState(false);
-        // const [index, setIndex] = useState(0);
-
-        const handleShow = () => {
-            setVisible(!isVisible);
-            props.addCount();
-        };
-
-        // const handleIndex = () => {
-        //     setIndex(currenIndex => currenIndex + 1);
-        // }
-        // const mainRef = useRef(null);
-        // useEffect(() => {
-        //     mainRef.current.focus();
-        // }, []);
-
-        return (
-            <div className="card animation">
-                <div className="word">{props.engVersion}</div>
-                <div className="transcription">{props.transcription}</div>
-                <button className={isVisible ? "btnVisible word" : "btnTranslate"} disabled={isVisible} onClick={handleShow} >
-                    {isVisible ? props.rusVersion : "Проверить"}
-                </button>
-            </div >
-        );
-    };
 
     return (
         <div className="card-container">
