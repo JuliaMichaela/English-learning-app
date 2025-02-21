c4add6105a98fe5156eb6aa6d40b2cc5c5dd0b4f
JuliaMichaela <juliaperez@yandex.ru>
2025-02-21 13:05:16 +0300

    initial commit

---
+++ src/App/App.jsx
@@ -0,0 +1,17 @@
+import Card from '../Components/Card/Card'
+import Footer from '../Components/Footer/Footer'
+import Header from '../Components/Header/Header'
+import './App.scss'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Card />
+      <Footer />
+    </>
+  )
+}
+
+export default App
