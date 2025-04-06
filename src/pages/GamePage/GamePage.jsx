3acde845a68f598c383098d79e92faaac4700e57
JuliaMichaela <juliaperez@yandex.ru>
2025-04-06 23:56:36 +0300

    created  GamePage.jsx

---
+++ src/pages/GamePage/GamePage.jsx
@@ -0,0 +1,36 @@
+import { useState } from 'react';
+import WordCard from "../../components/WordCard/WordCard";
+import './GamePage.scss'
+
+export default function GamePage({ upd }) {
+    const { words } = upd;
+    const [countSlider, setCountSlider] = useState(0);
+
+    function prevSlider() {
+        if (countSlider === 0) {
+            setCountSlider(words.length - 1);
+            return
+        }
+        setCountSlider((prevSlider) => prevSlider - 1);
+    }
+
+    function nextSlider() {
+        if (countSlider === words.length - 1) {
+            setCountSlider(0);
+            return
+        }
+        setCountSlider((prevSlider) => prevSlider + 1);
+    }
+
+
+    return (
+        <div className="container-wordList">
+
+            <button className="wordList-btn" onClick={prevSlider}>Prev</button>
+
+            <WordCard {...words[countSlider]} slideIndex={countSlider} />
+
+            <button className="wordList-btn" onClick={nextSlider}>Next</button>
+        </div>
+    );
+}
