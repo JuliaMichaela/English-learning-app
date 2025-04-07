372b7790e3b5e57c44974c0f8c3f3ab99d59e199
JuliaMichaela <juliaperez@yandex.ru>
2025-04-07 15:02:55 +0300

    updated style ErrorPage.jsx

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,13 +1,12 @@
 import { Link } from "react-router-dom";
 import style from './ErrorPage.module.scss';
-import foxError from '../../assets/images/foxError.png'
+import foxError from '../../assets/images/foxError.png';
 
 export default function ErrorPage() {
     return (
         <div className={style.error}>
-            <img className={style.fox__img} src={foxError} alt={foxError} />
             <div className={style.content}>
-                <h1>404</h1>
+                <img className={style.fox__img} src={foxError} alt={foxError} />
                 <p>Ой! Такой страницы не существует.</p>
                 <Link to="/" className={style.button}>Вернуться на главную</Link>
             </div>
