dc5a22be8da5490829689c0dd72f859f15015099
JuliaMichaela <juliaperez@yandex.ru>
2025-04-07 16:11:43 +0300

    updated style HomePage

--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,12 +1,16 @@
 import { Link } from "react-router-dom";
+import foxHello from '../../assets/images/fox-hello.jpg';
 import style from '../HomePage/HomePage.module.scss';
 
 export default function HomePage() {
     return (
-        <div className={style.container}>
-            <h1>Learning English Words</h1>
-            <p>Приложение для изучения иностранного языка и расширения словарного запаса.</p>
-            <Link to="/game">Старт</Link>
+        <div className={style.container__home}>
+            <div className={style.content}>
+                <img className={style.fox__img} src={foxHello} alt={foxHello} />
+                {/* <h1>Learning English Words</h1> */}
+                <p>Приложение для изучения английского языка и расширения словарного запаса</p>
+                <Link to="/game" className={style.button}>Старт</Link>
+            </div>
         </div>
     )
 }
